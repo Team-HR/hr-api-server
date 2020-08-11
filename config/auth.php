42967f50fc14df5c8292c09f9541688096f7ebb1
franz <franzval123@gmail.com>
2020-08-11 14:18:01 +0800

    Feat: Installed tymon/jwt-auth. API ready to use.

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api', //web
         'passwords' => 'users',
     ],
 
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt', //token
             'provider' => 'users',
             'hash' => false,
         ],
