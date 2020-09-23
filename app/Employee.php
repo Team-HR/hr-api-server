5e479e0281cd7e59ce1606c6087e87ad623fa256
franz <franzval123@gmail.com>
2020-09-23 15:20:45 +0800

    wip

--- app/Employee.php
+++ app/Employee.php
@@ -12,4 +12,40 @@ class Employee extends Model
         'middle_name',
         'ext_name'
     ];
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['full_name'];
+    /**
+     * Get the user's full name.
+     *
+     * @return string
+     */
+    public function getFullNameAttribute()
+    {
+        $firstName = $this->first_name;
+        $lastName = $this->last_name;
+
+        if ($this->middle_name == ".") {
+            $middleName = "";
+        } else {
+            $middleName    = $this->middle_name;
+            $middleName = $this->middle_name[0] . ".";
+        }
+
+        $extName    =    strtoupper($this->ext_name);
+        $exts = array('JR', 'SR');
+
+        if (in_array(substr($extName, 0, 2), $exts)) {
+            $extName = " " . mb_convert_case($extName, MB_CASE_UPPER, "UTF-8");
+        } else {
+            $extName = " " . $extName;
+        }
+
+        $fullname =  mb_convert_case("$lastName, $firstName $middleName", MB_CASE_UPPER, "UTF-8") . $extName;
+
+        return $fullname;
+    }
 }
