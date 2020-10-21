f94785dc41f31990981fdc4d63fe30f8ac14db70
fjavalencia <franzval123@gmail.com>
2020-10-21 14:21:10 +0800

    Fields: middle_name and ext_name should be nullable.

---
+++ database/migrations/2020_09_21_092144_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name', 100);
+            $table->string('first_name', 100);
+            $table->string('middle_name', 100)->nullable();
+            $table->string('ext_name', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
