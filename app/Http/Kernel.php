4d24ed317fbc2d8a4e027acc6175745ef15fe515
fjavalencia <franzval123@gmail.com>
2021-07-18 23:03:56 +0800

    Revert "Addition of Access-Control-Allow-Origin header response"
    This reverts commit 7c924f389c5b257cfd00abe0f967d4120fa99f3c.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'cors' => \App\Http\Middleware\Cors::class, 
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
